81927b3d66dd90274a6fa1ea259878435a426403
mas0061 <mas0061@gmail.com>
2015-03-09 06:59:41 +0900

    jestでテストするための枠組み作り

---
+++ src/test/js/CalendarTest.js
@@ -0,0 +1,9 @@
+jest.dontMock('../../main/jsx/components/Calendar');
+
+describe('Calendar', function() {
+  it('render calendar', function() {
+    var React = require('react/addons');
+    var Calendar = require('../../main/jsx/components/Calendar');
+    var TestUtils = React.addons.TestUtils;
+  });
+});
